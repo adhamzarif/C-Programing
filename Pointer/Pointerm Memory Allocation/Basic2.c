7e0232b7aea8b9506ddb4da3a9cbb6032adc7951
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 19:15:42 +0600

    Update Basic2.c

--- Pointer/Pointerm Memory Allocation/Basic2.c
+++ Pointer/Pointerm Memory Allocation/Basic2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int *ptr;
+    ptr = (int *)calloc(4,20);
+    // ptr = (int *)calloc(4,sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Memory Allocation Failed\n");
+    }
+    else
+    {
+        printf("Memory Allocation Successful\n");
+    }
+    return 0;
+}
