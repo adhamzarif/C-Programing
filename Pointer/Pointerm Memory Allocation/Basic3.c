d9f51503d5eac44f97148a1ddf5f4c7150933fa4
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 19:15:57 +0600

    Update Basic3.c

--- Pointer/Pointerm Memory Allocation/Basic3.c
+++ Pointer/Pointerm Memory Allocation/Basic3.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int *ptr;
+    ptr = (int *)calloc(4,20);
+    // ptr = (int *)calloc(4,sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Memory Allocation Failed\n");
+    }
+    else
+    {
+        printf("Memory Allocation Successful\n");
+        //do the work
+        free(ptr);
+        printf("Memory Freed Successful\n");
+    }
+    return 0;
+}
