69fb8103666fceca6ce68f81541e7731856dc94b
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 19:16:12 +0600

    Update Basic4.c

--- Pointer/Pointerm Memory Allocation/Basic4.c
+++ Pointer/Pointerm Memory Allocation/Basic4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int *ptr;
+    ptr = (int *)calloc(4,20);
+    // ptr = (int *)calloc(4,sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Memory Allocation Failed\n");
+    }
+    else
+    {
+        printf("Memory Allocation Successful\n");
+        //do the work
+        ptr = realloc(ptr,100);
+        printf("Memory Reallocation Successful\n");
+    }
+    return 0;
+}
