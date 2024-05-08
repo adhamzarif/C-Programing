0fc13ad94074bee992c6e74531b33c00c7079ae6
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:13:25 +0600

    Update Sum_Calculate2.c

--- Array/1D Array/Sum_Calculate2.c
+++ Array/1D Array/Sum_Calculate2.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+int main()
+{
+    int a[5],AB[5];
+    int i,j,B[5];
+    printf("Enter Value for A: ");
+    for(i = 0;i < 5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter Value for B: ");
+    for(i = 0;i < 5;i++)
+    {
+        scanf("%d",&B[i]);
+    }
+    for(i = 0;i < 5;i++)
+    {
+        AB[i] = a[i] + B[i];
+    }
+    for(i = 0;i < 5;i++)
+    {
+        printf("Sum is: %d\n",AB[i]);
+    }
+    
+    return 0;
+}
