81760820372ee5ffc1beea6bc13a4f4bad2ac2e2
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:12:50 +0600

    Update Sum_Calculate.c

--- Array/1D Array/Sum_Calculate.c
+++ Array/1D Array/Sum_Calculate.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main()
+{
+    int a[10],sum = 0;
+    int i;
+    for(i = 0;i < 10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i = 0;i < 10;i++)
+    {
+        sum = sum + a[i];
+    }
+    printf("Sum is: %d",sum);
+    return 0;
+}
