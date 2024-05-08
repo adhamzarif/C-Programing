d4ac041f5fc72a80e1160a387055c0fb068ff197
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:16:46 +0600

    Update Getting_input.c

--- Array/2D Array/Getting_input.c
+++ Array/2D Array/Getting_input.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+int main()
+{   
+    int i,j,n,m;
+    printf("Enter Row Number: ");
+    scanf("%d",&n);
+    printf("Enter Column Number: ");
+    scanf("%d",&m);
+    int a[n][m];
 
+        //Scanning
+    for(i = 0;i < n;i++)
+    {   
+        for(j = 0;j < m;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+        
+    }
+
+        //Printing
+    for(i = 0;i < n;i++)
+    {
+        for(j = 0;j < m;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
