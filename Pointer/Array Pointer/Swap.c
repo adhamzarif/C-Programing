e61b46a0bf7d266249b068d91f7b7ee597cf2df0
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 20:02:02 +0600

    Update Swap.c

--- Pointer/Array Pointer/Swap.c
+++ Pointer/Array Pointer/Swap.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int swap(int *x,int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+    return temp;
+}
+int main()
+{
+    int num1 = 10,num2 = 20;
+    swap(&num1,&num2);
+    printf("After swapping: %d %d",num1,num2);
+    return 0;
+}
 
