eef16b9383a58b8f470c1a13df3251b58eb2b355
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:06:52 +0600

    Update Basic_2.c

--- Array/Basic Array/Basic_2.c
+++ Array/Basic Array/Basic_2.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a[5] = {12, 11, 13, 20, 32};
 
+    printf("Value of index 0 is: %d\n",a[0]);
+    printf("Value of index 0 is: %d\n",a[3]);
+    return 0;
+}
