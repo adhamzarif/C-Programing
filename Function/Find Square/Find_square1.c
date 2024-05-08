669174f4404193baf2f50b5516c5793e7a7b5836
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 18:03:30 +0600

    Update Find_square1.c

--- Function/Find Square/Find_square1.c
+++ Function/Find Square/Find_square1.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int square(int a)
+{
+    return a * a;
+}
+int main()
+{
+    int num;
+    printf("Enter Numbers: ");
+    scanf("%d",&num);
+    printf("Square is: %d\n",square(num));
+    return 0;
+}
