3c0a95e0efaeb44ab33839e6711735f04dae409d
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 20:01:47 +0600

    Update Array_Sum.c

--- Pointer/Array Pointer/Array_Sum.c
+++ Pointer/Array Pointer/Array_Sum.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int sum(int *ptr,int x)
+{
+    int i,sum = 0;
+    for(i = 0;i < x;i++)
+    {
+        sum = sum + *(ptr + i);
+    }
+    return sum;
+}
+int main()
+{
+    int a[5] = {1, 2, 3, 4, 5};
+    int result = sum(a,5);
+    printf("Sum is: %d",result);
+    return 0;
+}
