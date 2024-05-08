b8f96a4ca67fceb6093bc3dcb5af911af27243ea
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 19:59:41 +0600

    Update AccessArrayElements.c

--- Pointer/Array Pointer/AccessArrayElements.c
+++ Pointer/Array Pointer/AccessArrayElements.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{   
+    int i,a[5] = {1, 2, 3, 4, 5};
+    int *ptr;
+    ptr = &a;
+    
+    for(i = 0;i < 5;i++)
+    {
+        printf("Array Elements are: %d\n",*ptr);
+        ptr++;
+    }
+
+    return 0;
+}
