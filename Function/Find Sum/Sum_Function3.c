d2a7e482b019bcdb05669824ce407d902c71911d
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 18:07:12 +0600

    Update Sum_Function3.c

--- Function/Find Sum/Sum_Function3.c
+++ Function/Find Sum/Sum_Function3.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int sum(int a,int b)
+{
+    return a + b;
+}
+int main()
+{
+    int num1,num2;
+    printf("Enter Numbers: ");
+    scanf("%d %d",&num1, &num2);
+    printf("Sum is: %d\n",sum(num1,num2));
+    return 0;
+}
