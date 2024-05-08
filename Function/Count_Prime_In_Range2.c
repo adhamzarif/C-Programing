4f994bc7eef6126a218c730781ba37f478e94601
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 18:01:19 +0600

    Update Count_Prime_In_Range2.c

--- Function/Count_Prime_In_Range2.c
+++ Function/Count_Prime_In_Range2.c
@@ -1,1 +1,29 @@
-
+#include <stdio.h>
+int primenumber(int x)
+{
+    int i;
+    for(i = 2;i < x / 2;i++)
+    {
+        if(x % i == 0)
+        {
+            return 1; //Not Prime
+        }
+    } return 0; //Prime
+} 
+int main()
+{
+    printf("Enter start number: ");
+    int i,strt,end,count = 0;
+    scanf("%d",&strt);
+    printf("Enter end number: ");
+    scanf("%d",&end);
+    for(i = strt;i <= end; i++)
+    {
+        if(primenumber(i) == 0)
+    {
+        count++;
+    }
+    }
+    printf("%d Prime Number\n",count);
+    return 0;
+}
