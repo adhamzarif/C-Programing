6a99beec9adeba5880dafdd675bcca55aeb2f321
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 17:58:55 +0600

    Update Area_Of_Triangle.c

--- Function/Area_Of_Triangle.c
+++ Function/Area_Of_Triangle.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+void numArray(int x[])
+{
+    int i;
+    for(i = 0;i < 5;i++)
+    {
+        printf("%d ",x[i]);
+    }
+}
+int main()
+{
+    int num[] = {10,20,30,40,50};
+    numArray(num);
+    return 0;
+}
