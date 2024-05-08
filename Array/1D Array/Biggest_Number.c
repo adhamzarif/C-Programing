8fcf45b316e8a85134610d9acc201f1017b0978a
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:09:33 +0600

    Update Biggest_Number.c

--- Array/1D Array/Biggest_Number.c
+++ Array/1D Array/Biggest_Number.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main()
+{
+    int a[7]= {-99, 45, 100, 37, 89, -327, 245},max = 0; //max = a[0]
+    int i;
+    for(i = 1;i < 7;i++)
+    {
+        if(a[i] < a[max]) //a[i] > max  //max
+        {
+            max = a[i];
+        };
+    }
+    
+    printf("Max is: %d",max);
+    return 0;
+}
