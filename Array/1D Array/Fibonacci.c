7224abf2874ac73481b1185c328c756e7c986fe2
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:10:45 +0600

    Update Fibonacci.c

--- Array/1D Array/Fibonacci.c
+++ Array/1D Array/Fibonacci.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int num,i;
+    printf("Enter terms: ");
+    scanf("%d",&num);
 
+    int arr[100];
+    arr[0] = 0;
+    arr[1] = 1;
+    for(i = 2;i < num;i++)
+    {
+        arr[i] =  arr[i-2] + arr[i-1];
+    }
+    printf("\n");
+    for(i = 0;i< num;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}
