4d18a6230a3dfa98fbbcc15a8fa98baff27453b6
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:12:02 +0600

    Update Linear_Search.c

--- Array/1D Array/Linear_Search.c
+++ Array/1D Array/Linear_Search.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int i,value,n,position = -1; //as because arrays starts from 0 thats why initialized from -1
+    printf("Enter value to search: ");
+    scanf("%d",&value);
+    int a[] = {10, 20, 30, 40, 50};
+    for(i = 0;i < 5;i++)
+    {
+        if(value == a[i])
+        {
+            position = i+1;
+            break;
+        }
+    }
+    if(position == -1)
+    {
+        printf("Not Found!!,Sorry\n");
+    }
+    else
+    {
+        printf("Yes!!,Found it %d at %d Position",value,position);
+    }
 
+    return 0;
+}
