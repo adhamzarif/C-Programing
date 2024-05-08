7e57be60a7ed47131b5fca5f91d5002aabdbd23b
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 20:01:33 +0600

    Update Array_Pointer2.c

--- Pointer/Array Pointer/Array_Pointer2.c
+++ Pointer/Array Pointer/Array_Pointer2.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main()
+{
+    int a[5] = {1, 2, 3, 4, 5};
+    int *ptr;
+    ptr = &a[3];
+    printf("%d\n",*(ptr));
+    printf("%d\n",*(ptr + 1));
+    printf("%d\n",*(ptr - 1));
+    return 0;
+}
