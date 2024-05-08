97c8458bb8e6dc983080af80dcd5c9391817dfef
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 19:15:25 +0600

    Update Basic1.c

--- Pointer/Pointerm Memory Allocation/Basic1.c
+++ Pointer/Pointerm Memory Allocation/Basic1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int *ptr;
+    ptr = (int *)malloc(20); //we are allocating a 20 size value on memory & declaring its type also converting it to pointer
+    // ptr = (int *)malloc(5 * sizeof(int));
+    if(ptr == NULL)
+    {
+        printf("Memory Allocation Failed\n");
+    }
+    else
+    {
+        printf("Memory Allocation Successful\n");
+    }
+    return 0;
+}
