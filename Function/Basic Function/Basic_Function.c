c622e448c0cae2fa944a3cb2ba873c61d3e610ab
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 18:09:12 +0600

    Create Basic_Function.c

---
+++ Function/Basic Function/Basic_Function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void say_hi() //void declares there is empty work which means it will not take any input/output
+{
+    printf("Hello,This is my first function");
+}
+int main()
+{
+    say_hi(); //Function call
+    return 0;
+}
