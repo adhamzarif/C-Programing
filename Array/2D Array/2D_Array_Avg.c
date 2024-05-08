f95cc6798194eb9c0685cc7cd05975d0eb8f6751
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:15:02 +0600

    Update 2D_Array_Avg.c

--- Array/2D Array/2D_Array_Avg.c
+++ Array/2D Array/2D_Array_Avg.c
@@ -1,1 +1,58 @@
-
+#include <stdio.h>
+int main()
+{   
+    int i,j,n,m;
+    printf("Enter Row Number: ");
+    scanf("%d",&n);
+    printf("Enter Column Number: ");
+    scanf("%d",&m);
+    int a[n][m],b[n][m];
+    //For A
+        //Scanning
+    for(i = 0;i < n;i++)
+    {   
+        for(j = 0;j < m;j++)
+        {
+            printf("A[%d][%d]: ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+        
+    }
+    //For B
+        //Scanning
+    for(i = 0;i < n;i++)
+    {   
+        for(j = 0;j < m;j++)
+        {
+            printf("B[%d][%d]: ",i,j);
+            scanf("%d",&b[i][j]);
+        }
+        
+    }
+    //Printing A
+        //Printing
+    printf("A: ");
+    for(i = 0;i < n;i++)
+    {
+        printf("\t");
+        for(j = 0;j < m;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    //Printing B
+        //Printing
+    printf("B: ");
+    for(i = 0;i < n;i++)
+    {
+        printf("\t");
+        for(j = 0;j < m;j++)
+        {
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
