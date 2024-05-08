ddc359c4d790cf0c89a0e1668d26a46229d74723
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 16:09:58 +0600

    Update Copy_All_Elements.c

--- Array/1D Array/Copy_All_Elements.c
+++ Array/1D Array/Copy_All_Elements.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int i,n,num;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    int a[n],b[n];
+//For A
+    printf("Enter numbers: ");
+    for(i = 0;i < n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Array of A: ");
+    for(i = 0;i < n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+//Replace A to B
+    for(i = 0;i < n;i++)
+    {
+        b[i] = a[i];
+    }
+    printf("Array of B: ");
+    for(i = 0;i < n;i++)
+    {
+        printf("%d ",b[i]);
+    }
 
+    return 0;
+}
