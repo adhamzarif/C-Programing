0b61d2693dabb1e17579f89a1df5a158c7b17638
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 19:59:59 +0600

    Update Array_Pointer.c

--- Pointer/Array Pointer/Array_Pointer.c
+++ Pointer/Array Pointer/Array_Pointer.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int main()
+{
+    int a[5] = {1, 2, 3, 4, 5};
+    int i,sum = 0;
+    for(i = 0;i < 5;i++)
+    {
+        //printf("%d ",*(a + i))
+        sum = sum + *(a + i);
+    }
+    printf("Sum is: %d",sum);
+    return 0;
+}
