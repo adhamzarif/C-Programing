c78e6b37f023c98b29ea7d9bcf61ef353359f719
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-09 15:43:17 +0600

    Create Call_By_Reference.c

---
+++ Function/Call_By_Reference.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void square(int *n)
+{
+    *n = (*n) * (*n);
+    printf("%d", *n);
+}
+int main()
+{
+    int number = 4;
+    square(&number);
+    printf("%d",number);
+    return 0;
+}
