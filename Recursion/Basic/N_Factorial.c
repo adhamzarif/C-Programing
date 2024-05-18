eeac26eaa4250c456e7901dd3b1d4cfb778046b2
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-18 17:37:20 +0600

    Update N_Factorial.c

--- Recursion/Basic/N_Factorial.c
+++ Recursion/Basic/N_Factorial.c
@@ -1,13 +1,5 @@
 # include <stdio.h>
 //function to print factorial of n
-int factorial(int n);
-int main() {
-int n;
-printf("enter n : ");
-scanf("%d", &n);
-printf("factorial is : %d", factorial(n));
-return 0;
-}
 int factorial(int n) {
 if(n == 0) {
 return 1;
@@ -16,3 +8,11 @@ int factnm1 = factorial(n-1);
 int factn = factnm1 * n;
 return factn;
 }
+int main() {
+int n;
+printf("enter n : ");
+scanf("%d", &n);
+printf("factorial is : %d", factorial(n));
+return 0;
+}
+
