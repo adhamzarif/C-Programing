d512443abcee185ac47207487619e066beddc8f7
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-18 17:15:33 +0600

    Update Basic_Struct.c

--- Structure/Basic/Basic_Struct.c
+++ Structure/Basic/Basic_Struct.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+struct Student
+{
+    int id;
+    double cgpa;
+    char name[100];
+}/*stu1*/ ; //we can declare a variable here too!!
+int main()
+{
+    struct Student s1; //declaring a veriable s1 for student structure
 
+    //Scanning
+    printf("Enter Your ID: \n");
+    scanf("%d",&s1.id);
+    printf("Enter Your CGPA: \n");
+    scanf("%lf",&s1.cgpa);
+    printf("Enter Your Name: \n");
+    scanf(" %[^\n]", s1.name); // Note the space before %[^\n] using
+
+    //Printing
+    printf("Your ID is: %d\n",s1.id);
+    printf("Your CGPA is: %lf\n",s1.cgpa);
+    printf("Your Name is: %s\n",s1.name);
+    return 0;
+}
