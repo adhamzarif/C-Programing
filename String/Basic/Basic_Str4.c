d5395aa0e52e076f7af890950d5e60615202f2b7
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-18 17:22:46 +0600

    Update Basic_Str4.c

--- String/Basic/Basic_Str4.c
+++ String/Basic/Basic_Str4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    char name[] = "Adham Zarif \Seam"; //Using line breaking charecter
+    char name[] = "Adham Zarif Seam"; //Using line breaking charecter
     
     printf("Answer is: %s",name);
     return 0;
